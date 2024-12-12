8d8ee439bbd676fbcb2a2dcb4a7c1ae8cba8f07b
Alejandro Cebrián Sánchez <Alejandro.Cebrian2@alu.uclm.es>
2024-12-12 14:26:52 +0100

    Cambios reunión

--- Assets/Scripts/SceneChanger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SceneChanger : MonoBehaviour {
-
-    // Método que cambia de escena al recibir el nombre de la escena
-    public void ChangeScene(string sceneName) { SceneManager.LoadScene(sceneName); }
-
-}
